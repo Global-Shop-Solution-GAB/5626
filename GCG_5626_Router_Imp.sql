5cad0464fe8a8eeacfa2dd4443a0c681d537c0bd
Service Web <sw@gssmail.com>
2024-03-14 13:52:42 -0500

    Commit changes in C:\Websites\Git\emarlow\5626\GCG_5626_Router_Imp.sql

---
+++ GCG_5626_Router_Imp.sql
@@ -0,0 +1,18 @@
+/* ==========================================
+ * TABLE: GCG_5626_Router_Imp
+ * ========================================== */
+CREATE TABLE "GCG_5626_Router_Imp"(
+ "routerName" CHAR(20),
+ "routerDescription" CHAR(30),
+ "routerWeight" CHAR(12),
+ "lineType" CHAR(1),
+ "partNumber" CHAR(20),
+ "workCenter" CHAR(4),
+ "lineDescription" CHAR(30),
+ "materialWeight" CHAR(12),
+ "qtyRuntime" CHAR(12),
+ "uploadUser" CHAR(9),
+ "uploadDate" CHAR(10),
+ "uploadCheck" BIT NOT NULL );
+
+
